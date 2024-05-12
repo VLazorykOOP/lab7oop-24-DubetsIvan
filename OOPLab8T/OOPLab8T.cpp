09293a19c1a2df26e8102d1082a58d024fd8976b
DubetsIvan <158566315+DubetsIvan@users.noreply.github.com>
2024-05-12 17:41:36 +0300

    .

--- OOPLab8T/OOPLab8T.cpp
+++ OOPLab8T/OOPLab8T.cpp
@@ -1,14 +1,67 @@
-﻿// OOPLab8T.cpp :  for Lab7 (new)
-//
+﻿#include <iostream>
+#include <queue>
 
-#include <iostream>
-#include "example.h"
-int main()
-{
-    int r;       
-    std::cout << "Lab 7 (new)\n";
-     std::cout << " Run example press 1 \n";
-     std::cin >> r; if (r == 1) example();
-    
+using namespace std;
+
+template <typename T>
+class ParametrizedQueue {
+private:
+    queue<T> elements;
+
+public:
+    void push(const T& item) {
+        elements.push(item);
+    }
+
+    void pop() {
+        if (!elements.empty()) {
+            elements.pop();
+        }
+        else {
+            cout << "Queue is empty. Cannot pop." << endl;
+        }
+    }
+
+    T front() const {
+        if (!elements.empty()) {
+            return elements.front();
+        }
+        else {
+            throw runtime_error("Queue is empty.");
+        }
+    }
+
+    bool empty() const {
+        return elements.empty();
+    }
+
+    int size() const {
+        return elements.size();
+    }
+};
+
+int main() {
+    ParametrizedQueue<int> intQueue;
+    intQueue.push(10);
+    intQueue.push(20);
+    intQueue.push(30);
+
+    while (!intQueue.empty()) {
+        cout << intQueue.front() << " ";
+        intQueue.pop();
+    }
+
+    cout << endl;
+
+    ParametrizedQueue<string> stringQueue;
+    stringQueue.push("Hello");
+    stringQueue.push("World");
+    stringQueue.push("!");
+
+    while (!stringQueue.empty()) {
+        cout << stringQueue.front() << " ";
+        stringQueue.pop();
+    }
+
+    return 0;
 }
-
